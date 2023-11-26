08e2606999c5db5af650d5585e04b0cf287ee7a5
David Calvo <david@dcalvo.io>
2023-11-26 13:38:50 -0500

    refactor data extract

--- data_capture.py
+++ data_capture.py
@@ -3,158 +3,115 @@ import torch
 import numpy as np
 
 from leap import HandType
-from leap.datatypes import Digit, Bone, Palm, Hand
+from leap.cstruct import LeapCStruct
+from leap.datatypes import Digit, Bone, Palm, Hand, Vector
 from leap.events import TrackingEvent
 from leap_motion_tracker import LeapMotionTracker
 
+hand_data_headers = [
+    # "id",
+    # "flags",
+    # "type",
+    "confidence",
+    # "visible_time",
+    "pinch_distance",
+    "grab_angle",
+    "pinch_strength",
+    "grab_strength",
+]
 
-def extract_bone_data(bone: Bone):
-    # Extract bone data
-    bone_prev_joint_x = bone.prev_joint.x
-    bone_prev_joint_y = bone.prev_joint.y
-    bone_prev_joint_z = bone.prev_joint.z
-    bone_next_joint_x = bone.next_joint.x
-    bone_next_joint_y = bone.next_joint.y
-    bone_next_joint_z = bone.next_joint.z
-    bone_width = bone.width
-    bone_rotation_x = bone.rotation.x
-    bone_rotation_y = bone.rotation.y
-    bone_rotation_z = bone.rotation.z
-    bone_rotation_w = bone.rotation.w
+palm_data_headers = [
+    "position.x",
+    "position.y",
+    "position.z",
+    "stabilized_position.x",
+    "stabilized_position.y",
+    "stabilized_position.z",
+    "velocity.x",
+    "velocity.y",
+    "velocity.z",
+    "normal.x",
+    "normal.y",
+    "normal.z",
+    "width",
+    "direction.x",
+    "direction.y",
+    "direction.z",
+    "orientation.x",
+    "orientation.y",
+    "orientation.z",
+    "orientation.w",
+]
 
-    data = [
-        bone_prev_joint_x,
-        bone_prev_joint_y,
-        bone_prev_joint_z,
-        bone_next_joint_x,
-        bone_next_joint_y,
-        bone_next_joint_z,
-        bone_width,
-        bone_rotation_x,
-        bone_rotation_y,
-        bone_rotation_z,
-        bone_rotation_w,
-    ]
+digit_data_headers = [
+    # "finger_id",
+    "is_extended",
+]
 
+bone_data_headers = [
+    "prev_joint.x",
+    "prev_joint.y",
+    "prev_joint.z",
+    "next_joint.x",
+    "next_joint.y",
+    "next_joint.z",
+    "width",
+    "rotation.x",
+    "rotation.y",
+    "rotation.z",
+    "rotation.w",
+]
+
+
+def extract_data(struct: LeapCStruct, headers: list[str]):
+    """
+    Extract data from a LeapCStruct using a list of headers.
+    If the struct is nested, the headers can be nested using dot notation.
+    e.g. "position.x" will extract the x value from the position vector.
+    :param struct: struct to extract data from
+    :param headers: list of headers to extract
+    :return: list of extracted data
+    """
+    data = []
+    for header in headers:
+        root = struct
+        while "." in header:
+            root_header, sub_header = header.split(".", maxsplit=1)
+            root = getattr(root, root_header)
+            header = sub_header
+        data.append(getattr(root, header))
     return data
 
 
 def extract_digit_data(digit: Digit):
-    # Extract digit data
-    digit_id = digit.finger_id
-    digit_is_extended = digit.is_extended
-
-    # Extract metacarpal data
-    metacarpal_data = extract_bone_data(digit.metacarpal)
-
-    # Extract proximal data
-    proximal_data = extract_bone_data(digit.proximal)
-
-    # Extract intermediate data
-    intermediate_data = extract_bone_data(digit.intermediate)
-
-    # Extract distal data
-    distal_data = extract_bone_data(digit.distal)
-
-    data = [
-        # digit_id,
-        digit_is_extended,
-        *metacarpal_data,
-        *proximal_data,
-        *intermediate_data,
-        *distal_data,
-    ]
-
-    return data
-
-
-def extract_palm_data(palm: Palm):
-    # Extract palm data
-    palm_position_x = palm.position.x
-    palm_position_y = palm.position.y
-    palm_position_z = palm.position.z
-    palm_stabilized_position_x = palm.stabilized_position.x
-    palm_stabilized_position_y = palm.stabilized_position.y
-    palm_stabilized_position_z = palm.stabilized_position.z
-    palm_velocity_x = palm.velocity.x
-    palm_velocity_y = palm.velocity.y
-    palm_velocity_z = palm.velocity.z
-    palm_normal_x = palm.normal.x
-    palm_normal_y = palm.normal.y
-    palm_normal_z = palm.normal.z
-    palm_width = palm.width
-    palm_direction_x = palm.direction.x
-    palm_direction_y = palm.direction.y
-    palm_direction_z = palm.direction.z
-    palm_orientation_x = palm.orientation.x
-    palm_orientation_y = palm.orientation.y
-    palm_orientation_z = palm.orientation.z
-    palm_orientation_w = palm.orientation.w
-
-    data = [
-        palm_position_x,
-        palm_position_y,
-        palm_position_z,
-        palm_stabilized_position_x,
-        palm_stabilized_position_y,
-        palm_stabilized_position_z,
-        palm_velocity_x,
-        palm_velocity_y,
-        palm_velocity_z,
-        palm_normal_x,
-        palm_normal_y,
-        palm_normal_z,
-        palm_width,
-        palm_direction_x,
-        palm_direction_y,
-        palm_direction_z,
-        palm_orientation_x,
-        palm_orientation_y,
-        palm_orientation_z,
-        palm_orientation_w,
-    ]
-
-    return data
-
-
-def extract_hand_data(hand: Hand):
-    # Extract hand data
-    hand_id = hand.id
-    hand_flags = hand.flags
-    hand_type = hand.type
-    hand_confidence = hand.confidence
-    hand_visible_time = hand.visible_time
-    hand_pinch_distance = hand.pinch_distance
-    hand_grab_angle = hand.grab_angle
-    hand_pinch_strength = hand.pinch_strength
-    hand_grab_strength = hand.grab_strength
-
-    data = [
-        # hand_id,
-        # hand_flags,
-        # hand_type,
-        hand_confidence,
-        # hand_visible_time,
-        hand_pinch_distance,
-        hand_grab_angle,
-        hand_pinch_strength,
-        hand_grab_strength,
-    ]
-
-    return data
+    """
+    Extract data from a digit. This includes the digit data and each bone data.
+    :param digit: digit to extract data from
+    :return: list of extracted data
+    """
+    digit_data = extract_data(digit, digit_data_headers)
+    for bone in digit.bones:
+        bone_data = extract_data(bone, bone_data_headers)
+        digit_data.extend(bone_data)
+    return digit_data
 
 
 def extract_event_data(event: TrackingEvent):
+    """
+    Extract data from a TrackingEvent. Flatten the data from each hand, palm, digit and bone.
+    :param event: event to extract data from
+    :return: list of extracted data
+    """
     for hand in event.hands:
         # Ignore left hand (for now)
         if hand.type == HandType.Left:
             continue
 
         # Extract hand data
-        hand_data = extract_hand_data(hand)
+        hand_data = extract_data(hand, hand_data_headers)
 
         # Extract palm data
-        palm_data = extract_palm_data(hand.palm)
+        palm_data = extract_data(hand.palm, palm_data_headers)
 
         # Extract thumb data
         thumb_data = extract_digit_data(hand.thumb)
@@ -171,6 +128,7 @@ def extract_event_data(event: TrackingEvent):
         # Extract pinky data
         pinky_data = extract_digit_data(hand.pinky)
 
+        # Flatten data
         data = [
             *hand_data,
             *palm_data,
@@ -182,6 +140,11 @@ def extract_event_data(event: TrackingEvent):
         ]
 
         return data
+
+
+def roll_up_event_data(event_data: list):
+    # Roll up hand data
+    pass
 
 
 def main():
@@ -204,8 +167,10 @@ def main():
 
                 data = extract_event_data(tracker.most_recent_event)
                 if data:
-                    data = torch.tensor(data)
-                    print(data, len(data))
+                    print(len(data))
+                    print(data)
+
+
     except Exception as e:
         raise e
     finally:
